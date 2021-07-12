fd5b086351e055282491c1a1195c8f8fe3d24baa
marianovatri <marianovatri@gmail.com>
2021-07-12 19:32:29 -0300

    TransferenciaAReact

---
+++ Calculadora/src/App.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+
+
+  return (
+    
+    <div className="App">
+     
+             
+                  
+       
+       
+       <label for="">Calculadora Avanzada</label> 
+
+       
+       <input type="number" /> 
+       <button >Clear</button>
+
+       
+
+       
+       <input  type="number" /> 
+       <button>Clear</button> 
+      
+     
+       
+
+       <h2>Calculadora en base a los 2 inputs: </h2>
+       <p>la suma es =  </p>
+       <p>La resta es =  </p>
+       <p>La multiplicacion es =  </p>
+       <p>La division es =  </p>
+       <p>La potencia es =  </p>
+       <p>La raiz cuadrada es =  </p>
+       <p>La hipotenusa es =  </p>
+                   
+                             
+       
+       
+  
+    
+    </div>
+    
+    
+  );
+  
+  }
+
+
+  
+export default App;
