2772b254c2c28f1ed8f1847b201cf046640ed43d
marianovatri <marianovatri@gmail.com>
2021-07-13 14:38:05 -0300

    UseEffect&Css

--- Calculadora/src/App.js
+++ Calculadora/src/App.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, {useEffect ,useState } from 'react';
 import './App.css';
-import {Suma,Resta, Multiplicacion,Division} from './Calculos';
+
 
 
 function App() {
@@ -10,7 +10,22 @@ function App() {
     num1: "0" ,
     num2: "0" 
   })
+  const [suma, setSuma] = useState()
+  const [resta, setResta] = useState()
+  const [mult, setMult] = useState()
+  const [div, setDiv] = useState()
+ 
+  
 
+  useEffect(()=>{
+    const { num1, num2 } = datos
+    setSuma(Number(num1) + Number(num2))
+    setResta(Number(num1) - Number(num2))
+    setMult(Number(num1) * Number(num2))
+    setDiv(Number(num1) / Number(num2))
+  },[datos])
+
+  
   const handleInput = (valor) => {
     setDatos({...datos, [valor.target.name] : valor.target.value })
   }
@@ -24,32 +39,37 @@ function App() {
              
                   
        
-       
-       <label>Calculadora Avanzada</label> 
-
-       <form >
+     <label>Calculadora Avanzada</label> 
+      
+      <div className="contenedor">
+        <form >
 
           <div >
               <input type="number" placeholder="0" onChange={handleInput} name="num1"></input>
-              <button type="reset" >Clear</button>
+              <button type="reset">Clear</button>
           </div>
 
-          <div >
+        </form>
+
+        <form >
+          <div className="Calculo">
               <input type="number" placeholder="0" onChange={handleInput} name="num2"></input>
               <button type="reset" >Clear</button>
           </div>
-
+          </form>
+      </div>
+         
           <h2>Calculadora en base a los 2 inputs: </h2>
-           <p>la suma es = {Suma}  </p>
-           <p>La resta es = {Resta}   </p>
-           <p>La multiplicacion es = {Multiplicacion}   </p>
-           <p>La division es =  {Division}  </p>
+           <p>la suma es = {suma}  </p>
+           <p>La resta es = {resta}   </p>
+           <p>La multiplicacion es = {mult}   </p>
+           <p>La division es =  {div}  </p>
            <p>La potencia es =  </p>
            <p>La raiz cuadrada es =  </p>
            <p>La hipotenusa es =  </p>
 
 
-        </form>
+        
 
 
     
@@ -64,4 +84,3 @@ function App() {
   
  
 export default  App
-
