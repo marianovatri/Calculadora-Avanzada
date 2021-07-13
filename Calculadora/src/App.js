7ec312bcc29b94a36f0ea11ff8e24b5533ffaa45
marianovatri <marianovatri@gmail.com>
2021-07-12 21:40:31 -0300

    UsandoState

--- Calculadora/src/App.js
+++ Calculadora/src/App.js
@@ -2,8 +2,17 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+ 
 
+  const [datos, setDatos] = useState({
+    num1: "0" ,
+    num2: "0" 
+  })
 
+  const handleInput = (valor) => {
+    setDatos({...datos, [valor.target.name] : valor.target.value })
+  }
+  
   return (
     
     <div className="App">
@@ -12,34 +21,32 @@ function App() {
                   
        
        
-       <label for="">Calculadora Avanzada</label> 
+       <label>Calculadora Avanzada</label> 
 
-       
-       <input type="number" /> 
-       <button >Clear</button>
+       <form >
 
-       
+          <div >
+              <input type="number" placeholder="0" onChange={handleInput} name="num1"></input>
+              <button type="reset" >Clear</button>
+          </div>
 
-       
-       <input  type="number" /> 
-       <button>Clear</button> 
-      
-     
-       
+          <div >
+              <input type="number" placeholder="0" onChange={handleInput} name="num2"></input>
+              <button type="reset" >Clear</button>
+          </div>
 
-       <h2>Calculadora en base a los 2 inputs: </h2>
-       <p>la suma es =  </p>
-       <p>La resta es =  </p>
-       <p>La multiplicacion es =  </p>
-       <p>La division es =  </p>
-       <p>La potencia es =  </p>
-       <p>La raiz cuadrada es =  </p>
-       <p>La hipotenusa es =  </p>
-                   
-                             
-       
-       
-  
+          <h2>Calculadora en base a los 2 inputs: </h2>
+           <p>la suma es = {parseFloat(datos.num1)+parseFloat(datos.num2)}  </p>
+           <p>La resta es = {parseFloat(datos.num1)-parseFloat(datos.num2)}   </p>
+           <p>La multiplicacion es = {parseFloat(datos.num1)*parseFloat(datos.num2)}   </p>
+           <p>La division es =  {parseFloat(datos.num1)/parseFloat(datos.num2)}  </p>
+           <p>La potencia es =  </p>
+           <p>La raiz cuadrada es =  </p>
+           <p>La hipotenusa es =  </p>
+
+
+        </form>
+
     
     </div>
     
@@ -47,7 +54,7 @@ function App() {
   );
   
   }
-
-
   
-export default App;
+ 
+export default  App
+
